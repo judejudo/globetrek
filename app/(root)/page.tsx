a41910ff431080c736364fba2592c891f361f8e7
judejudo <ocomilj@gmail.com>
2025-03-10 08:28:55 +0300

    root page corrections

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HiOutlineArrowRight } from "react-icons/hi";
 import Image from "next/image";
-import Link from "next/link";
+
 
 const Page = () => {
   return (
